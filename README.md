6659b5cae5c4099d6cd32bb7b1da694bca85724f
egin10 <egin066@gmail.com>
2023-10-12 12:06:52 +0700

    import json

---
+++ README.md
@@ -0,0 +1,12 @@
+# IMPORT JSON TO FIREBASE
+The project is simple way to importing data into Firebase.
+The project is based on this [link](https://cherylm.hashnode.dev/importing-json-data-into-firestore) and have modified based on needs.
+
+### Usage
+1. Clone the project.
+2. Install dependencies using the command `yarn` or `npm install`.
+3. Change collection on file `data.json` based on your data collections.
+4. Running the project with the command `npm index.js` to push data collections to Firebase.
+5. Done :)
+
+Thank you 🚀
--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-const { initializeApp } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-require('dotenv').config();
-const data = require('./data.json');
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+require("dotenv").config();
+const data = require("./data.json");
+
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_PUBLIC_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+// };
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyCrgD2cmF9zVXLvtuTcUZvv5fbmYYc6nR8",
+  authDomain: "quiz-app-c8a4f.firebaseapp.com",
+  projectId: "quiz-app-c8a4f",
+  storageBucket: "quiz-app-c8a4f.appspot.com",
+  messagingSenderId: "590275545476",
+  appId: "1:590275545476:web:1b34bd3eb793bba7b775d1",
+  measurementId: "G-6VYB189Y6B",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const importJSON = async () => {
-  for await (const user of data) {
-    db.collection('users').doc().set(user);
+  for await (const quiz of data) {
+    // insert correct answer
+    quiz.incorrect_answers.push(quiz.correct_answer);
+    // shuffle answer
+    quiz.incorrect_answers = shuffle(quiz.incorrect_answers);
+
+    // insert to Firestore
+    db.collection("quiz").doc().set(quiz);
   }
+
+  console.log(data.length);
+  console.log("Done🚀");
+};
+
+const shuffle = (array) => {
+  return array.sort(() => Math.random() - 0.5);
 };
 
 importJSON();
